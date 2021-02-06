7b11c8e980c9a79eaa2e8c1c8f8ebe978b929b69
byLLIPyT <byllipyt@icloud.com>
2021-02-06 13:48:15 +0300

    initialCommit

---
+++ Pryaniky/TextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextTableViewCell.swift
+//  Pryaniky
+//
+//  Created by Александр Уткин on 06.02.2021.
+//  Copyright © 2021 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
